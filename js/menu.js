9b800fadc00a84872fed5617658048f6e9f6b35d
Oleh Novosad <onovosad321@gmail.com>
2025-05-11 20:20:30 +0200

    comm

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-  }
-})();
+    const refs = {
+      openModalBtn: document.querySelector("[data-menu-open]"),
+      closeModalBtn: document.querySelector("[data-menu-close]"),
+      modal: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleMenu);
+    refs.closeModalBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.modal.classList.toggle("is-open");
+    }
+  })();
